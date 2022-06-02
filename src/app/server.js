01122fc49f95ddac10a4b63b7aa27a635677042e
JUSSIAR <klokov.stas.022@mail.ru>
2022-06-03 02:31:26 +0300

    Removed extra import

--- src/app/server.js
+++ src/app/server.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import {postPageAdapter} from "../pages/post-page/post-page-adapter.js";
-import multipleFetch from 'multiple-fetch'
 
 const app = express()
 
